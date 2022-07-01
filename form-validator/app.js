7f85dd8f3720c5544ce36f16877154bb7b637ff1
EmpAsh938 <ashokthapa938@gmail.com>
2022-07-01 21:15:40 +0545

    input field check fixed

--- form-validator/app.js
+++ form-validator/app.js
@@ -6,75 +6,75 @@ const confirm_password = document.getElementById('cpassword');
 const button = document.querySelector('.submit-button');
 
 const re =
-/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 
-let validUsername = true;
-let validPassword = true;
-let validEmail = true;
-let validConfirmPassword = true;
+let validUsername = false;
+let validPassword = false;
+let validEmail = false;
+let validConfirmPassword = false;
 
-function toggleError(elem,show) {
-    if(show) elem.classList.add('active');
+function toggleError(elem, show) {
+    if (show) elem.classList.add('active');
     else elem.classList.remove('active');
 }
 
-function validateUsername(){
-    if(username.value.length < 3) {
-        toggleError(username.nextElementSibling,true);
-        toggleError(username,true);
+function validateUsername() {
+    if (username.value.length < 3) {
+        toggleError(username.nextElementSibling, true);
+        toggleError(username, true);
         validUsername = false;
     } else {
-        toggleError(username,false);
-        toggleError(username.nextElementSibling,false);
+        toggleError(username, false);
+        toggleError(username.nextElementSibling, false);
         validUsername = true;
     }
 }
 
-function validateEmail(){
-    if(!email.value.match(re)){
+function validateEmail() {
+    if (!email.value.match(re)) {
         toggleError(email.nextElementSibling, true);
         toggleError(email, true);
         validEmail = false;
     } else {
-        toggleError(email,false);
-        toggleError(email.nextElementSibling,false);
+        toggleError(email, false);
+        toggleError(email.nextElementSibling, false);
         validEmail = true;
     }
 }
 
-function validatePassword(){
-    if(password.value.length < 6) {
-        toggleError(password,true);
+function validatePassword() {
+    if (password.value.length < 6) {
+        toggleError(password, true);
         toggleError(password.nextElementSibling, true);
         validPassword = false;
     } else {
-        toggleError(password,false);
-        toggleError(password.nextElementSibling,false);
+        toggleError(password, false);
+        toggleError(password.nextElementSibling, false);
         validPassword = true;
     }
-    
+
 }
 
-function validateConfirmPassword(){
-    if(!confirm_password.value || password.value !== confirm_password.value) {
-        toggleError(confirm_password,true);
-        toggleError(confirm_password.nextElementSibling,true);
+function validateConfirmPassword() {
+    if (!confirm_password.value || password.value !== confirm_password.value) {
+        toggleError(confirm_password, true);
+        toggleError(confirm_password.nextElementSibling, true);
         validConfirmPassword = false;
     } else {
-        toggleError(confirm_password,false);
-        toggleError(confirm_password.nextElementSibling,false);
+        toggleError(confirm_password, false);
+        toggleError(confirm_password.nextElementSibling, false);
         validConfirmPassword = true;
     }
-    
+
 }
 
 
 
-function submitForm(e){
+function submitForm(e) {
     e.preventDefault();
-    
-    if(validUsername && validPassword && validEmail && validConfirmPassword) {
+
+    if (validUsername && validPassword && validEmail && validConfirmPassword) {
         console.log("Successful Submission");
     }
 }
